a7c8de5f7c520e2e366658eb8d1c11e233c1f0db
vamsikrish98 <vamsikrisshna06@gmail.com>
2026-07-22 06:09:40 +0530

    Implement SQL query for conversion data retrieval
    Add SQL query to select and format conversion data

--- models/staging/stg_conversions.sql
+++ models/staging/stg_conversions.sql
@@ -1,1 +1,8 @@
-
+SELECT
+    conversion_id,
+    customer_id,
+    campaign_id,
+    PARSE_DATE('%m/%d/%Y', conversion_date) AS conversion_date,
+    LOWER(TRIM(conversion_type)) AS conversion_type,
+    CAST(revenue AS NUMERIC) AS revenue
+FROM {{ ref('conversions') }}
