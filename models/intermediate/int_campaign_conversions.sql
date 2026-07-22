457865e4fbcf4a13d8e9caaa462b6353a5b8ee8c
vamsikrish98 <vamsikrisshna06@gmail.com>
2026-07-22 23:30:50 +0530

    Update int_campaign_conversions.sql

--- models/intermediate/int_campaign_conversions.sql
+++ models/intermediate/int_campaign_conversions.sql
@@ -1,1 +1,19 @@
+SELECT
+    campaign_id,
 
+    COUNT(DISTINCT conversion_id) AS total_conversions,
+
+    COUNT(DISTINCT customer_id) AS converted_customers,
+
+    SUM(revenue) AS total_revenue,
+
+    AVG(revenue) AS avg_revenue,
+
+    MIN(conversion_date) AS first_conversion_date,
+
+    MAX(conversion_date) AS latest_conversion_date
+
+FROM {{ ref('stg_conversions') }}
+
+GROUP BY
+    campaign_id
