f78f5d6d9f98edc590510339a99b4b848180f27b
vamsikrish98 <vamsikrisshna06@gmail.com>
2026-07-22 06:11:38 +0530

    Add SQL query to select campaign metrics from meta_ads

--- models/staging/stg_meta_ads.sql
+++ models/staging/stg_meta_ads.sql
@@ -1,1 +1,9 @@
-
+SELECT
+    campaign_id,
+    LOWER(TRIM(campaign_name)) AS campaign_name,
+    CAST(cost AS NUMERIC) AS spend,
+    CAST(impressions AS INT64) AS impressions,
+    CAST(clicks AS INT64) AS clicks,
+    PARSE_DATE('%m/%d/%Y', date) AS campaign_date,
+    'meta' AS channel
+FROM {{ ref('meta_ads') }}
