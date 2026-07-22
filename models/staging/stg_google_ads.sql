b007e2c23882f63d180f6b5d9b4f16b992a0fe18
vamsikrish98 <vamsikrisshna06@gmail.com>
2026-07-22 23:26:38 +0530

    Refactor SQL query to use SAFE_CAST and CAST

--- models/staging/stg_google_ads.sql
+++ models/staging/stg_google_ads.sql
@@ -1,9 +1,9 @@
 SELECT
-    campaign_id,
+    CAST(campaign_id AS STRING) AS campaign_id,
     LOWER(TRIM(campaign_name)) AS campaign_name,
-    CAST(cost AS NUMERIC) AS spend,
-    CAST(impressions AS INT64) AS impressions,
-    CAST(clicks AS INT64) AS clicks,
-    PARSE_DATE('%m/%d/%Y', date) AS campaign_date,
+    SAFE_CAST(spend AS NUMERIC) AS spend,
+    SAFE_CAST(impressions AS INT64) AS impressions,
+    SAFE_CAST(clicks AS INT64) AS clicks,
+    SAFE.PARSE_DATE('%m/%d/%Y', CAST(`date` AS STRING)) AS campaign_date,
     'google' AS channel
 FROM {{ ref('google_ads') }}
